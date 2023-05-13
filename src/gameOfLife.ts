fb1ed83ea3440f026f12a95eb7a39ba356391192
Marco Fratini <marcofratini@outlook.com>
2023-05-13 13:56:38 +0200

    Fix message return in printBoard

--- src/gameOfLife.ts
+++ src/gameOfLife.ts
@@ -25,7 +25,7 @@ export const generateGameBoardWithRandomCells = (
   for (let row = 1; row <= rows; row++) {
     const rowLocation = [];
     for (let column = 1; column <= columns; column++) {
-      if (Math.random() < 0.4) {
+      if (Math.random() < 0.3) {
         const cell = {
           alive: true,
           row: row,
@@ -46,4 +46,19 @@ export const generateGameBoardWithRandomCells = (
   }
 
   return boardLogic;
+};
+
+export const printBoard = (board: object[][]) => {
+  let message = "";
+  for (let row = 0; row < board.length; row++) {
+    for (let column = 0; column < board[row].length; column++) {
+      if (board[row][column].alive) {
+        message += " 1 ";
+      } else {
+        message += " 0 ";
+      }
+    }
+    message += "\n";
+  }
+  return message;
 };
