808b6c7e6cbaecc1948f97bf89ff6f2d5689d0d9
Marco Fratini <marcofratini@outlook.com>
2023-05-13 16:10:22 +0200

    Add testing condition for generateboard function

--- src/gameOfLife.test.ts
+++ src/gameOfLife.test.ts
@@ -2,7 +2,7 @@ import { generateGameBoard } from "./gameOfLife";
 
 describe("Given the function generateGameBoard", () => {
   describe("When given the row value of 2 and the column value of 2", () => {
-    test("Then if should return a board with 2 columns and 2 rows", () => {
+    test("Then it should return a board with 2 columns and 2 rows", () => {
       const rows = 2;
       const columns = 2;
 
@@ -17,6 +17,18 @@ describe("Given the function generateGameBoard", () => {
         ],
       ];
 
+      const resultReturned = generateGameBoard(rows, columns);
+
+      expect(resultReturned).toEqual(expectedResult);
+    });
+  });
+
+  describe("When given the row value 0 and column value 0", () => {
+    test("Then it should return a []", () => {
+      const rows = 0;
+      const columns = 0;
+
+      const expectedResult: [] = [];
       const resultReturned = generateGameBoard(rows, columns);
 
       expect(resultReturned).toEqual(expectedResult);
