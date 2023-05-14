4469538267113bdc0ece2b02bd0c91a322727fd8
Marco Fratini <marcofratini@outlook.com>
2023-05-14 13:07:06 +0200

    Expand place cell on board function

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { Cell } from "./cellInterface.js";
+import { generateGameBoard } from "./generateBoardFunctions.js";
 
 const generateUserGameBoard = (rows: number, columns: number) => {
   const gameTable = document.querySelector(".container-table") as Element;
@@ -9,6 +10,11 @@ const generateUserGameBoard = (rows: number, columns: number) => {
 
     for (let column = 1; column <= columns; column++) {
       rowTable.innerHTML += `<div class="col row-${row}-col-${column}"></div>`;
+      const colPart = document.querySelector(`.row-${row}-col-${column}`);
+
+      for (let button = 0; button < 1; button++) {
+        colPart.innerHTML += `<button id="${row} "value="${column}" class="cell-button"></button>`;
+      }
     }
   }
 };
@@ -21,7 +27,40 @@ const placeCellOnBoard = (
   const cellToPlace = document.querySelector(
     `.row-${row}-col-${column}`
   ) as Element;
-  cellToPlace.classList.add("alive");
+  if (!currentBoard[row - 1][column - 1].alive) {
+    currentBoard[row - 1][column - 1].alive = true;
+
+    cellToPlace.classList.remove("dead");
+    cellToPlace.classList.add("alive");
+  } else if (currentBoard[row - 1][column - 1].alive) {
+    currentBoard[row - 1][column - 1].alive = false;
+
+    cellToPlace.classList.remove("alive");
+    cellToPlace.classList.add("dead");
+  }
 };
 
-generateUserGameBoard(100, 100);
+const registerEventListeners = (currentBoard: Cell[][]) => {
+  console.log(currentBoard);
+  const cells = document.querySelectorAll(".cell-button");
+
+  cells.forEach((cell) => {
+    cell.addEventListener("click", () => {
+      console.log(cell);
+      const row = parseInt(cell.id);
+      const column = parseInt(cell.value);
+      console.log(row, column);
+      placeCellOnBoard(row, column, currentBoard);
+    });
+  });
+};
+
+const startGame = () => {
+  const rows = 5;
+  const columns = 5;
+  const currentBoard = generateGameBoard(rows, columns);
+  generateUserGameBoard(rows, columns);
+  registerEventListeners(currentBoard);
+};
+
+startGame();
