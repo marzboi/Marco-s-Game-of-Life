cc4b2d657ba6834733da425eb2ff2ced2f23d536
Marco Fratini <marcofratini@outlook.com>
2023-05-14 17:07:25 +0200

    Add testing function for applyRules

---
+++ dist/app/applyRules.test.js
@@ -0,0 +1,24 @@
+import { applyRules } from "./applyRules";
+import { checkNeighboard } from "./checkNeighbour";
+import { generateGameBoard } from "./generateBoardFunctions";
+describe("Given the function applyRules", () => {
+    describe("When given the board with all 0", () => {
+        test("Then it should return the same board", () => {
+            const board = generateGameBoard(2, 2);
+            const neighboardCells = checkNeighboard(board);
+            applyRules(board, neighboardCells);
+            const expectResult = [
+                [
+                    { alive: false, row: 1, col: 1 },
+                    { alive: false, row: 1, col: 2 },
+                ],
+                [
+                    { alive: false, row: 2, col: 1 },
+                    { alive: false, row: 2, col: 2 },
+                ],
+            ];
+            const resultReturned = board;
+            expect(resultReturned).toEqual(expectResult);
+        });
+    });
+});
